87825f8c439ec3321f9cba7b41136bb948880caf
Aneeshie <aneeshdas556@gmail.com>
2026-04-25 00:16:39 +0530

    add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Button } from "./components/ui/button";
+
 function App() {
-  return <div className="bg-red-500">hello world</div>;
+  return (
+    <>
+    <div className="bg-red-500">hello world</div>
+    <Button>Test</Button>
+    </>
+
+  );
 }
 
 export default App;
